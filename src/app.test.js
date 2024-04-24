c1d7469f47f86daef1059a60daa92fefd6f8c281
Star2578 <nuttykingger@gmail.com>
2024-04-24 14:54:33 +0700

    add config.js

--- src/app.test.js
+++ src/app.test.js
@@ -7,5 +7,5 @@ test('getDay returns the long-format day of the week', () => {
 });
 test('getDay returns the month of the year', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('March');
+    expect( day ).toBe('November');
 });
