86a5f50985a3ec8bb88d21e7011322ddcf889b9c
Star2578 <nuttykingger@gmail.com>
2024-04-24 14:46:07 +0700

    add monthOfTheYear

--- src/app.test.js
+++ src/app.test.js
@@ -5,3 +5,7 @@ test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
 });
+test('getDay returns the month of the year', () => {
+    const day = dayOfTheWeek( new Date('3/11/2020') );
+    expect( day ).toBe('March');
+});
